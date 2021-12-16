73fd09eac4250f959f80d34e6bb01a93704cb7db
dewisibbald <153200@csgaugustinus.nl>
2021-12-16 14:19:10 +0000

    x

--- JS/test.js
+++ JS/test.js
@@ -1,25 +1,36 @@
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
+  canvas = createCanvas(600,300);
   canvas.parent('processing');
-  //noLoop();
-}
-
-function TekenKerk(hoogte) {
-  noStroke();
-  fill('purple');
-  rect(0,350,75,hoogte);
-  fill('white');
-  triangle(0,350,75,350,37,310);
 }
 
 function draw() {
   noStroke();
-  TekenKerk(300);
-  translate(100,-100);
-  TekenKerk(275);
-  translate(100,-100);
-  TekenKerk(350);
+  background('cyan');
+  
+  if (mouseY > 200) {
+    fill('orange');
+  }
+  else {
+    fill('yellow');
+  }
 
+  ellipse(width/2,200,200);
+  fill('green');
+  rect(0,200,600);
+
+for (var n = 0; < aantal; n++) {
+  tekenHuis();
+  translate();
+}
+
+function tekenHuis {
+  push();
+  translate(40,100);
+  fill('red');
+  triangle(40,0,0,50,80,50);
+  fill('grey');
+  rect(0,50,80,80);
+  pop();
+  }
 }
 
